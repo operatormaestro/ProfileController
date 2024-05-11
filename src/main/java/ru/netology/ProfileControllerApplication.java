4badbcf8c79f9cee03d36c1a6ea43a4db5c83c62
Mikhail Semavin <operator.mixa@mail.ru>
2024-05-11 14:56:44 +0300

    "Latest version"

--- src/main/java/ru/netology/ProfileControllerApplication.java
+++ src/main/java/ru/netology/ProfileControllerApplication.java
@@ -7,22 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/")
+
 public class ProfileControllerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProfileControllerApplication.class, args);
     }
 
-    private final SystemProfile profile;
-
-    public ProfileControllerApplication(SystemProfile profile) {
-        this.profile = profile;
-    }
-
-    @GetMapping("profile")
-    public String getProfile() {
-        return profile.getProfile();
-    }
 }
